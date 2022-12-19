599249ffca2a1437f40c5eaa25892d080d418bc6
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2022-12-18 23:32:44 -0500

    Add acc module Att1

---
+++ js/accessibility.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function () {
+    const options = {
+      labels: {
+        resetTitle: 'Resetear valores',
+        closeTitle: 'Cerrar',
+        menuTitle: 'Accesibilidad',
+        increaseText: 'Aumentar tamaño del texto',
+        decreaseText: 'Reducir tamaño del texto',
+        increaseTextSpacing: 'Aumentar espaceado del texto',
+        decreaseTextSpacing: 'Reducir espaceado del texto',
+        increaseLineHeight: 'Aumentar interlineado',
+        decreaseLineHeight: 'Reducir interlineado',
+        invertColors: 'Invertir colores (Alto contraste)',
+        grayHues: 'Escala de grises',
+        underlineLinks: 'Destacar hipervínculos',
+        bigCursor: 'Cursor grande',
+        readingGuide: 'Guía de lectura',
+        textToSpeech: 'Texto a voz',
+        speechToText: 'Voz a texto',
+        disableAnimations: 'Deshabilitar animaciones'
+      },
+      customFunctions: [
+        {
+          method: (cf, state) => {
+            window.localStorage.clear();
+            window.location.reload();
+          },
+          buttonText: 'Valores por defecto',
+          id: 1,
+          toggle: false,
+          icon: 'restore_alt',
+          emoji: '❓'
+        }
+      ],
+      animations: {
+        buttons: false
+      }
+    };
+    new Accessibility(options);
+  }, false);
