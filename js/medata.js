c448f90a3316a55d862261a55b680d6b79ac21b6
CarlosJaramilloDev <carlosjaramillodev@gmail.com>
2023-02-01 01:06:40 -0500

    Fix hr width Att1

--- js/medata.js
+++ js/medata.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   let hrs = document.querySelectorAll('hr.separator, hr.contact-separator');
   hrs.forEach((hr) => {
     const brother = hr.previousElementSibling;
-    const syle = getComputedStyle(brother);∫
+    const syle = getComputedStyle(brother);
     hr.style.setProperty('--hrsize', syle.width);
   });
 }, false);
